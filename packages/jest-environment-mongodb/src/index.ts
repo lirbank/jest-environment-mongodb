5a7f37838679f8fc185f83df9dc17f6e89258435
Mikael Lirbank <mikael@airlab.io>
2019-07-31 14:39:18 -0700

    Enable tests on replica sets (WIP)

--- packages/jest-environment-mongodb/src/index.ts
+++ packages/jest-environment-mongodb/src/index.ts
@@ -1,6 +1,6 @@
 import { Config as JestConfig } from '@jest/types';
 import NodeEnvironment from 'jest-environment-node';
-import { MongoMemoryServer } from 'mongodb-memory-server';
+import { MongoMemoryReplSet, MongoMemoryServer } from 'mongodb-memory-server';
 import { MongoMemoryServerOptsT } from 'mongodb-memory-server-core/lib/MongoMemoryServer';
 
 declare global {
@@ -9,6 +9,7 @@ declare global {
       MONGO_URI: string;
       MONGO_DB_NAME: string;
       MONGOD: MongoMemoryServer;
+      // MONGO_REPL_SET: MongoMemoryReplSet;
     }
   }
 }
@@ -22,9 +23,11 @@ export type MongoDbEnvironmentConfig = JestConfig.ProjectConfig & {
 // Use a single shared mongod instance when Jest is launched with the
 // --runInBand flag
 let mongod: MongoMemoryServer | null = null;
+let replSet: MongoMemoryReplSet | null = null;
 
 export default class MongoDbEnvironment extends NodeEnvironment {
   private readonly mongod: MongoMemoryServer;
+  private readonly replSet: MongoMemoryReplSet;
 
   constructor(config: MongoDbEnvironmentConfig) {
     super(config);
@@ -34,17 +37,42 @@ export default class MongoDbEnvironment extends NodeEnvironment {
         mongod = new MongoMemoryServer(config.testEnvironmentOptions);
       }
 
+      if (!replSet) {
+        replSet = new MongoMemoryReplSet({
+          binary: {
+            version: '4.0.4',
+          },
+          instanceOpts: [{ storageEngine: 'wiredTiger' }],
+        });
+      }
+
       this.mongod = mongod;
+      this.replSet = replSet;
     } else {
       this.mongod = new MongoMemoryServer(config.testEnvironmentOptions);
+
+      this.replSet = new MongoMemoryReplSet({
+        binary: {
+          version: '4.0.4',
+        },
+        instanceOpts: [{ storageEngine: 'wiredTiger' }],
+      });
     }
 
     this.global.MONGOD = this.mongod;
   }
 
   public async setup() {
-    this.global.MONGO_URI = await this.mongod.getConnectionString();
-    this.global.MONGO_DB_NAME = await this.mongod.getDbName();
+    await this.replSet.waitUntilRunning();
+    // Need to wait due to:
+    // https://github.com/nodkz/mongodb-memory-server/issues/74
+    // https://github.com/nodkz/mongodb-memory-server/issues/166
+    // await new Promise(r => setTimeout(r, 2000));
+
+    this.global.MONGO_URI = await this.replSet.getConnectionString();
+    this.global.MONGO_DB_NAME = await this.replSet.getDbName();
+    // this.global.MONGO_URI = await this.mongod.getConnectionString();
+    // this.global.MONGO_DB_NAME = await this.mongod.getDbName();
 
     await super.setup();
   }
@@ -52,6 +80,7 @@ export default class MongoDbEnvironment extends NodeEnvironment {
   public async teardown() {
     if (!this.runInBand) {
       await this.mongod.stop();
+      await this.replSet.stop();
     }
 
     await super.teardown();
