00768d8c0b3b22e376b98fa43a6f6ea3bb7fd6d0
Mikael Lirbank <mikael@airlab.io>
2019-08-20 00:43:12 -0700

    Enable tests on replica sets (WIP)

--- packages/jest-environment-mongodb/src/index.ts
+++ packages/jest-environment-mongodb/src/index.ts
@@ -1,6 +1,7 @@
 import { Config as JestConfig } from '@jest/types';
 import NodeEnvironment from 'jest-environment-node';
 import { MongoMemoryReplSet, MongoMemoryServer } from 'mongodb-memory-server';
+import { MongoMemoryReplSetOptsT } from 'mongodb-memory-server-core/lib/MongoMemoryReplSet';
 import { MongoMemoryServerOptsT } from 'mongodb-memory-server-core/lib/MongoMemoryServer';
 
 declare global {
@@ -8,71 +9,66 @@ declare global {
     interface Global {
       MONGO_URI: string;
       MONGO_DB_NAME: string;
-      MONGOD: MongoMemoryServer;
-      // MONGO_REPL_SET: MongoMemoryReplSet;
+      MONGOD: MongoMemoryServer | MongoMemoryReplSet;
     }
   }
 }
 
-export type MongoDbEnvironmentConfig = JestConfig.ProjectConfig & {
-  testEnvironmentOptions?:
-    | JestConfig.ProjectConfig['testEnvironmentOptions']
-    | MongoMemoryServerOptsT;
-};
+type ServerOpts = Partial<MongoMemoryServerOptsT> & { standalone?: true };
+type ReplSetOpts = Partial<MongoMemoryReplSetOptsT> & { standalone: false };
+type TestEnvironmentOptions = ServerOpts | ReplSetOpts;
 
-// Use a single shared mongod instance when Jest is launched with the
-// --runInBand flag
-let mongod: MongoMemoryServer | null = null;
-let replSet: MongoMemoryReplSet | null = null;
+export interface MongoDbEnvironmentConfig extends JestConfig.ProjectConfig {
+  testEnvironmentOptions: TestEnvironmentOptions;
+}
+
+function isReplSetOpts(opts: TestEnvironmentOptions): opts is ReplSetOpts {
+  return opts.standalone === false;
+}
+
+function isReplSet(
+  server: MongoMemoryServer | MongoMemoryReplSet,
+): server is MongoMemoryReplSet {
+  return typeof Reflect.get(server, 'waitUntilRunning') === 'function';
+}
+
+// Use a single shared mongod instance (or replica set) when Jest is launched
+// with the --runInBand flag
+let mongod: MongoMemoryServer | MongoMemoryReplSet | null = null;
 
 export default class MongoDbEnvironment extends NodeEnvironment {
-  private readonly mongod: MongoMemoryServer;
-  private readonly replSet: MongoMemoryReplSet;
+  private readonly mongod: MongoMemoryServer | MongoMemoryReplSet;
 
   constructor(config: MongoDbEnvironmentConfig) {
     super(config);
 
+    const opts = config.testEnvironmentOptions;
+    const Server = isReplSetOpts(opts) ? MongoMemoryReplSet : MongoMemoryServer;
+
     if (this.runInBand) {
       if (!mongod) {
-        mongod = new MongoMemoryServer(config.testEnvironmentOptions);
-      }
-
-      if (!replSet) {
-        replSet = new MongoMemoryReplSet({
-          binary: {
-            version: '4.0.4',
-          },
-          instanceOpts: [{ storageEngine: 'wiredTiger' }],
-        });
+        mongod = new Server(opts);
       }
 
       this.mongod = mongod;
-      this.replSet = replSet;
     } else {
-      this.mongod = new MongoMemoryServer(config.testEnvironmentOptions);
-
-      this.replSet = new MongoMemoryReplSet({
-        binary: {
-          version: '4.0.4',
-        },
-        instanceOpts: [{ storageEngine: 'wiredTiger' }],
-      });
+      this.mongod = new Server(opts);
     }
 
     this.global.MONGOD = this.mongod;
   }
 
   public async setup() {
-    await this.replSet.waitUntilRunning();
     // Need to wait due to:
     // https://github.com/nodkz/mongodb-memory-server/issues/74
     // https://github.com/nodkz/mongodb-memory-server/issues/166
     // await new Promise(r => setTimeout(r, 2000));
+    if (isReplSet(this.mongod)) {
+      await this.mongod.waitUntilRunning();
+    }
 
-    this.global.MONGO_URI = await this.replSet.getConnectionString();
-    this.global.MONGO_DB_NAME = await this.replSet.getDbName();
-    // this.global.MONGO_URI = await this.mongod.getConnectionString();
-    // this.global.MONGO_DB_NAME = await this.mongod.getDbName();
+    this.global.MONGO_URI = await this.mongod.getConnectionString();
+    this.global.MONGO_DB_NAME = await this.mongod.getDbName();
 
     await super.setup();
   }
@@ -80,7 +76,6 @@ export default class MongoDbEnvironment extends NodeEnvironment {
   public async teardown() {
     if (!this.runInBand) {
       await this.mongod.stop();
-      await this.replSet.stop();
     }
 
     await super.teardown();
